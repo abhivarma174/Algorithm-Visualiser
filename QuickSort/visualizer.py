2426981c7ea614d94f08efad5e12a3b57c3a05b8
Miss_Supriya <52968975+khssupriya@users.noreply.github.com>
2021-01-02 23:24:08 +0530

    add: quick sort visualizer

--- QuickSort/visualizer.py
+++ QuickSort/visualizer.py
@@ -1,4 +1,46 @@
-import random
+import pygame, random
+
+WIDTH = 1280
+HEIGHT = 720
+GAP = 2
+
+ARRSIZE = 80
+RECTW = WIDTH / ARRSIZE - GAP
+arr = [random.randrange(50, 500) for i in range(80)]
+
+pygame.init()
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Quick Sort')
+
+# ---------------------------
+
+def displayArr(pivot):
+
+    '''
+    Displays the array as bars of height equal to the value
+    '''
+
+    window.fill((0, 0, 0)) 
+
+    for i in range(len(arr)):
+
+        if i == pivot:
+            color = (0, 255, 0) # green
+        else:
+            color = (255, 0, 0) # red
+
+        # (x-cord, y-cord, width, height)
+        pygame.draw.rect(window, color, (i * (RECTW + GAP), HEIGHT - arr[i], RECTW, arr[i]))
+
+    pygame.display.update()
+    pygame.time.wait(20)
+
+def logArr():
+    for val in arr:
+        print(val, end = ' ')
+    print()
+
+# --------------------------------
 
 def quickSort(arr, start , stop):
 
@@ -50,16 +92,30 @@ def partition(arr, start, stop):
         if arr[j] <= arr[pivot]:
             i = i + 1
             arr[i] , arr[j] = arr[j] , arr[i]
+            displayArr(pivot)
 
     #swap arr[i] and arr[pivot]
     arr[pivot] , arr[i + 1] = arr[i + 1] , arr[pivot]
 
     pivot = i + 1
     return pivot
- 
+
+# --------------------------------
+
+
 # Driver Code
 if __name__ == "__main__":
-    arr = [random.randrange(10, 100) for i in range(10)]
-    quickSort(arr, 0, len(arr) - 1)
-    print(arr)
-    
+    running = True
+    firstTime = True
+    while running:
+
+        window.fill((0, 0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        if firstTime:
+            quickSort(arr, 0, ARRSIZE - 1)
+        
+        firstTime = False
